3345c77421747a3341cb1512d7a991b4c16ade06
LoicDerrieux <l.derrieux@fondespierre.com>
2017-04-05 11:10:09 +0200

    ajout test rest / json

--- serveur.js
+++ serveur.js
@@ -2,5 +2,13 @@ var express = require('express');
 var app = express(); 
 app.get('/', function (req, res) { 
 res.sendfile(__dirname + '/views/index.html'); 
-}); 
+});
+app.get('/client', function(request,response){
+	var object = {
+		"nom" : "derrieux",
+		"prenom" : "loic"
+	};
+	response.setHeader('Content-Type', 'application/json');
+    response.send(JSON.stringify(object));
+} );
 app.listen(process.env.PORT);
