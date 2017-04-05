03437fef5efa1bd9b3d2ba67a4cc6d2431f9b36d
LoicDerrieux <l.derrieux@fondespierre.com>
2017-04-05 09:32:16 +0200

    init

---
+++ serveur.js
@@ -0,0 +1,18 @@
+var http = require('http'); 
+var serveur = http.createServer(function(req, res) { 
+    res.writeHead(200, {"Content-Type": "text/html"}); 
+    res.write
+('<!doctype html>' 
++'<html lang="fr">' 
++'<head>' 
++'<meta Charest="utf-8">' 
++'<title>nodeJS</title>' 
++'</head>' 
++'<body>' 
++'<h1>Hello World! Ceci est un titre</h1>' 
++'<p>Ceci est un <strong>paragraphe</strong>.</p>' 
++'</body>' 
++'</html>');
+res.end();
+}); 
+serveur.listen(8080);
