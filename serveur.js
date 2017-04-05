6754dbb7dc7ecbe8a2b45a906c660316500ff7c1
LoicDerrieux <l.derrieux@fondespierre.com>
2017-04-05 10:56:23 +0200

    switch to express

--- serveur.js
+++ serveur.js
@@ -1,18 +1,6 @@
-var http = require('http'); 
-var serveur = http.createServer(function(req, res) { 
-    res.writeHead(200, {"Content-Type": "text/html"}); 
-    res.write
-('<!doctype html>' 
-+'<html lang="fr">' 
-+'<head>' 
-+'<meta Charest="utf-8">' 
-+'<title>nodeJS</title>' 
-+'</head>' 
-+'<body>' 
-+'<h1>Hello World! Ceci est un titre</h1>' 
-+'<p>Ceci est un <strong>paragraphe</strong>.</p>' 
-+'</body>' 
-+'</html>');
-res.end();
+var express = require('express'); 
+var app = express(); 
+app.get('/', function (req, res) { 
+res.sendfile(__dirname + '/views/index.html'); 
 }); 
-serveur.listen(80);
+app.listen(80);
