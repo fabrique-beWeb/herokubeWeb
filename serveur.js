1781a802f7086b5e16bc1459b7052cb6976d76fc
LoicDerrieux <l.derrieux@fondespierre.com>
2017-04-05 10:25:35 +0200

    switch to https

--- serveur.js
+++ serveur.js
@@ -1,4 +1,4 @@
-var http = require('http'); 
+var http = require('https'); 
 var serveur = http.createServer(function(req, res) { 
     res.writeHead(200, {"Content-Type": "text/html"}); 
     res.write
