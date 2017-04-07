6123c5a6c1ad73a13f86f81faca133bba66d1a4f
LoicDerrieux <l.derrieux@fondespierre.com>
2017-04-07 16:21:26 +0200

    ajout database

--- serveur.js
+++ serveur.js
@@ -2,45 +2,52 @@ var express = require('express');
 var sqlite3 = require('sqlite3').verbose(); 
 
 //Database
-var setRet = function (err,row){
-	retour.push("hello");
-};
- 
-var retour = {};
+var db = new sqlite3.Database('database/database');
+
+
 //serveur web
 var app = express(); 
+
 app.get('/', function (req, res) { 
 	res.sendFile(__dirname + '/views/index.html'); 
 });
+
 app.get('/client', function(request,response){
 	var object = {
 		"nom" : "derrieux",
 		"prenom" : "loic"
 	};
 	response.setHeader('Content-Type', 'application/json');
-    response.json(object);
+	response.json(object);
 } );
-app.get('/sqlite',function(req,res){
-	
-	var test = "";
-	var i = 0;
-	var db = new sqlite3.Database(':memory:');
 
-db.serialize(function() {
-  db.run("CREATE TABLE lorem (info TEXT)");
-
-  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-  for (var i = 0; i < 10; i++) {
-      stmt.run("Ipsum " + i);
-  }
-  stmt.finalize();
-
-  db.each("SELECT rowid AS id, info FROM lorem;",setRet);
+app.get('/sqlite3',function(req,res){
+	db.all("SELECT rowid AS id, info FROM lorem;",function(err,rows){
+		res.setHeader('Content-Type', 'application/json');
+		res.json(rows); 
+	});
 });
 
-db.close();
 
-	res.setHeader('Content-Type', 'application/json');
-	res.json(retour);
+app.post('/init',function(req,res){
+	db.serialize(function() {
+		db.run("DROP TABLE IF EXISTS lorem");
+		db.run("CREATE TABLE IF NOT EXISTS lorem (info TEXT)");
+
+		var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+		for (var i = 0; i < 10; i++) {
+			stmt.run("hello !!!!!" + i);
+		}
+		stmt.finalize();
+		
+
+	}); 
+	res.redirect('/sqlite3');
+	//res.status(200).send();
 });
+
+
+
+
+
 app.listen(process.env.PORT || 12107);
